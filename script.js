64a20943925dc9dc1fc52b4ea7de3662a3196c1e
AACHOURMOHAMED <92208712+AACHOURMOHAMED@users.noreply.github.com>
2022-06-13 11:52:42 +0100

    Fix backgound menu fix space between the nav elements Fix javascript linters

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-//hamburger menu
+// hamburger menu
 const hamburger = document.querySelector('.hamburger');
 const mobileMenu = document.querySelector('.mobile-menu');
 
 hamburger.addEventListener('click', () => {
-hamburger.classList.toggle('active');
-mobileMenu.classList.toggle('active');
+  hamburger.classList.toggle('active');
+  mobileMenu.classList.toggle('active');
 });
 
 document.querySelectorAll('.menu-item').forEach((n) => n.addEventListener('click', () => {
-hamburger.classList.remove('active');
-mobileMenu.classList.remove('active');
+  hamburger.classList.remove('active');
+  mobileMenu.classList.remove('active');
 }));
